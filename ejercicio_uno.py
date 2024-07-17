98d3c69f103bfa22b3b69c73f47f6823f51ec62f
ashlyhuamani <ashlyhuamaniatiquipa.@gmail.com>
2024-07-17 10:51:56 -0500

    solucion:ejerccio 1

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,14 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+def encontrar_menor_mayor(lista):
+    if not lista:
+        return "La lista está vacía"
+    
+    diccionario = {"menor": min(lista), "mayor": max(lista)}
+    return diccionario
+
+
+entrada = [2, 8, 10, 5, 1, 0]
+resultado = encontrar_menor_mayor(entrada)
+print(resultado)
